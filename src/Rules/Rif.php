3247d3f154b8f3a7b85bbd193c1927cfec9f9d78
Wilsen Hernández <wilsenh95@gmail.com>
2021-02-17 22:31:34 -0400

    🚧 Iniciado la lógica de la regla de validación

---
+++ src/Rules/Rif.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Wilsenhc\ValidarRif\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Rif implements Rule
+{
+    /** @var string */
+    protected $attribute;
+
+    /**
+     * Determinar si el RIF en cuestion es valido.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        $this->attribute = $attribute;
+
+        // Verificar si el RIF tiene el formato valido
+        if (!preg_match('/^[VEPJGC]-?[\d]{8}-?[\d]$/i', $value))
+        {
+            return false;
+        }
+
+        $rif = strtoupper($value);
+        $rif = str_replace('-', '', $value);
+
+        // TODO: Añadir lógica para calcular el digito verificador.
+        return true;
+    }
+
+    /**
+     * Obtener el mensaje de error de la validación.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'El RIF no es válido';
+    }
+}
