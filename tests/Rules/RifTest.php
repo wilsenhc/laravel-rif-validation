04a278835e390cec5601ea144595bf76a3e30f3c
Wilsen Hernández <wilsenh95@gmail.com>
2021-02-21 11:43:07 -0400

    🔨 Add setUp method to RifTest

--- tests/Rules/RifTest.php
+++ tests/Rules/RifTest.php
@@ -9,6 +9,21 @@ use Wilsenhc\RifValidation\RifValidationServiceProvider;
 
 class RifTest extends TestCase
 {
+    /**
+     * @var  \Wilsenhc\RifValidation\Rules\Rif  $rule
+     */
+    protected $rule;
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->rule = new Rif();
+    }
+
     protected function getPackageProviders($app)
     {
         return [
@@ -19,21 +34,20 @@ class RifTest extends TestCase
     /** @test */
     public function it_will_return_true_if_rif_is_valid()
     {
-        $rule = new Rif();
-
         // Valid RIF of "Universidad de Carabobo"
         $rif = 'G-20000041-4';
 
-        $this->assertTrue($rule->passes('rif', $rif));
+        $this->assertTrue($this->rule->passes('rif', $rif));
 
         // Valid RIF of "Banesco Banco Universal"
         $rif = 'J-07013380-5';
 
-        $this->assertTrue($rule->passes('rif', $rif));
+        $this->assertTrue($this->rule->passes('rif', $rif));
 
         // Valid RIF of "Nicolas Maduro Moros"
         $rif = 'V-05892464-0';
 
-        $this->assertTrue($rule->passes('rif', $rif));
+        $this->assertTrue($this->rule->passes('rif', $rif));
+    }
     }
 }
