bdb2c5a5ac0ca23d44ba67597532211537ef89d3
Wilsen Hernández <wilsenh95@gmail.com>
2023-01-22 19:05:09 -0400

    Add PHP 8.2 and Laravel 10 support

--- src/RifValidationServiceProvider.php
+++ src/RifValidationServiceProvider.php
@@ -11,7 +11,7 @@ class RifValidationServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../lang' => $this->app->langPath().'/vendor/validateRif',
+            __DIR__ . '/../lang' => $this->app->langPath() . '/vendor/validateRif',
         ]);
 
         $this->loadTranslationsFrom(__DIR__.'/../lang/', 'validateRif');
