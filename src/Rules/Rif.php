94a506667d09f705bc9c97c02982f151c89819f2
Wilsen Hernández <13445515+wilsenhc@users.noreply.github.com>
2025-04-24 19:30:51 -0400

    feat: upgrade to support laravel 11 and 12

--- src/Rules/Rif.php
+++ src/Rules/Rif.php
@@ -2,104 +2,40 @@
 
 namespace Wilsenhc\RifValidation\Rules;
 
-use Illuminate\Contracts\Validation\Rule;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Support\Str;
+use Wilsenhc\RifValidation\RifValidator;
 
-class Rif implements Rule
+class Rif implements ValidationRule
 {
     /** @var string */
     protected $attribute;
 
-    /** @var array */
-    protected $nationality_array;
-
-    /** @var array */
-    protected $multipliers;
+    /** @var \Wilsenhc\RifValidation\RifValidator */
+    protected RifValidator $validator;
 
     public function __construct()
     {
-        $this->nationality_array = [
-            'V' => '1',
-            'E' => '2',
-            'J' => '3',
-            'P' => '4',
-            'G' => '5',
-            'C' => '3',
-        ];
-
-        $this->multipliers = [4, 3, 2, 7 ,6, 5, 4, 3, 2];
+        $this->validator = new RifValidator();
     }
 
     /**
-     * Determine if the RIF validation rule passes.
+     * Run the validation rule.
      *
      * @param  string  $attribute
      * @param  mixed  $value
-     * @return bool
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
      */
-    public function passes($attribute, $value): bool
+    public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         $this->attribute = $attribute;
 
-        $value = Str::upper($value);
-
-        // Verificar si el RIF tiene el formato valido
-        if (!preg_match('/^[VEPJGC]-?[\d]{8}-?[\d]$/i', $value))
-        {
-            return false;
+        if ($this->validator->isValid($value)) {
+            return;
         }
 
-        $full_rif = strtoupper($value);
-        $full_rif = str_replace('-', '', $value);
-
-        $contributor = substr($full_rif, 0, -1);
-        $validationNumber = substr($full_rif, -1, 1);
-
-        return $this->validationNumber($contributor) == $validationNumber;
-    }
-
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message(): string
-    {
-        return __('validateRif::messages.rif', [
-            'attribute' => $this->attribute,
-        ]);
-    }
-
-    /**
-     * Calcate the Validation Number
-     *
-     * @param  string  $rif
-     * @return string
-     */
-    private function validationNumber($rif): string
-    {
-        // FIRST STEP:
-        // Replace the letter for its numeric value.
-        $rif[0] = $this->nationality_array[$rif[0]];
-
-        $split_rif = str_split($rif);
-
-        // SECOND STEP
-        // Multiply each value by its *constant* multiplier from the multipiers
-        // array.
-        $sum = 0;
-        foreach ($split_rif as $index => $value)
-        {
-            $sum += intval($value) * $this->multipliers[$index];
-        }
-
-        // THIRD STEP
-        $remainder = intval($sum % 11);
-
-        // FOURTH STEP
-        $difference = 11 - $remainder;
-
-        // FINAL STEP
-        return ($difference > 9) ? '0' : strval($difference);
+        $fail(Str::replace(':attribute', $this->attribute, __('validateRif::messages.rif')));
     }
 }
